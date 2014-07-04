4ebc73151c0ae36f5d22b9bc886c97419abdda00
Aleksey Yablokov <ya_al@bk.ru>
2014-07-04 11:58:29 +0400

    GitFetchAllRepos выполняет fetch в отдельных потоках

---
+++ GitFetchAllRepos/src/Fetcher.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.Callable;
+
+/**
+ * Поток, выполняющий fetch.
+ */
+public class Fetcher implements Callable<String> {
+    private static final String GIT_EXECUTABLE = getGitExecutable();
+    private static final String command = GIT_EXECUTABLE + " fetch --all --prune ";
+
+    private final File repoDir;
+
+    public Fetcher(File repoDir) {
+        this.repoDir = repoDir;
+    }
+
+    @Override
+    public String call() throws Exception {
+        String output = repoDir.getAbsolutePath() + ":\n";
+        try {
+            Process process = Runtime.getRuntime().exec(command, null, repoDir);
+            int status = process.waitFor();
+            if (status == 0) {
+                output += inputStreamToString(process.getInputStream());
+            } else {
+                throw new RuntimeException();
+            }
+        } catch (IOException | InterruptedException e) {
+            output += e.getMessage();
+        }
+        return output;
+    }
+
+    private String inputStreamToString(InputStream io) throws IOException {
+        StringBuilder result = new StringBuilder();
+        InputStreamReader isr = new InputStreamReader(io);
+        char[] c = new char[1];
+        while (isr.read(c) != -1) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+
+    /**
+     * Написать для разных ОС.
+     */
+    public static String getGitExecutable() {
+        return "/usr/bin/git";
+    }
+}
--- GitFetchAllRepos/src/GitFetchAllRepos.java
+++ GitFetchAllRepos/src/GitFetchAllRepos.java
@@ -2,52 +2,71 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
- * Вызывает команду fetch для git-репозитариев, перечисленных в repo_urls_for_fetch.txt
+ * Вызывает команду fetch для git-репозитариев, перечисленных в repo_urls_for_fetch.txt (в отдельных потоках).
  */
 public class GitFetchAllRepos {
     private static final File REPO_URLS = new File("repo_urls_for_fetch.txt");
-    private static final String GIT_EXECUTABLE = getGitExecutable();
 
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.printf("Read: %s\n\n", REPO_URLS.getAbsolutePath());
+        List<File> repos = readRepos();
+        List<Fetcher> fetchers = makeFetchers(repos);
+        List<Future<String>> undoneFutures = executeFetchers(fetchers);
+        printOutput(undoneFutures);
+    }
+
+    private static void printOutput(List<Future<String>> undoneFutures) throws InterruptedException {
+        while (!undoneFutures.isEmpty()) {
+            List<Future<String>> done = new ArrayList<>();
+            for (Future<String> future : undoneFutures) {
+                try {
+                    if (future.isDone()) {
+                        done.add(future);
+                        System.out.printf("%s%n", future.get());
+                    }
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+            undoneFutures.removeAll(done);
+        }
+    }
+
+    private static List<File> readRepos() throws IOException {
+        List<File> repos = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(REPO_URLS));
         String line;
         while ((line = reader.readLine()) != null) {
             File repoDir = new File(line);
-            System.out.printf("Fetch: %s\n", repoDir.getAbsolutePath());
             if (!repoDir.exists()) {
                 throw new IllegalArgumentException("Repo dir doesn't exists: " + repoDir.getAbsolutePath());
             }
-            String command = GIT_EXECUTABLE + " fetch --all --prune ";
-            Process process = Runtime.getRuntime().exec(command, null, repoDir);
-            int status = process.waitFor();
-            if (status == 0) {
-                reprintOutput(process);
-            } else {
-                throw new RuntimeException();
-            }
-            System.out.println();
+            repos.add(repoDir);
         }
+        return repos;
     }
 
-    /**
-     * Перенаправляет поток вывода запущенного процесса (git) в консоль.
-     */
-    private static void reprintOutput(Process process) throws IOException {
-        InputStream is = process.getInputStream();
-        int b;
-        while ((b = is.read()) != -1) {
-            System.out.write(b);
+    private static List<Fetcher> makeFetchers(List<File> repos) {
+        List<Fetcher> fetchers = new ArrayList<>();
+        for (File repoDir : repos) {
+            fetchers.add(new Fetcher(repoDir));
         }
+        return fetchers;
     }
 
-    /**
-     * Написать для разных ОС.
-     */
-    public static String getGitExecutable() {
-        return "/usr/bin/git";
+    private static List<Future<String>> executeFetchers(List<Fetcher> fetchers) throws InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(fetchers.size());
+        List<Future<String>> futures = es.invokeAll(fetchers);
+        es.shutdown();
+        return futures;
     }
+
 }
