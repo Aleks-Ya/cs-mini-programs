a8b264633ac4d2828dc7cd7c22bf7825a61886a5
Aleksey Yablokov <ya_al@bk.ru>
2014-10-24 12:48:21 +0400

    Версия 2.4: Инсталлятор под Linux

--- GitFetchAllRepos/src/Main.java
+++ GitFetchAllRepos/src/Main.java
@@ -4,7 +4,6 @@ import config.JarSiblingConfig;
 import java.io.IOException;
 
 /**
- * todo Инсталлятор под Linux
  * todo Определять ситуацию, когда не введен пароль в ssh-add
  * todo Выводить информацию по каждому репозитарию по мере завершения его задачи
  * todo Добавить логирование в файл
